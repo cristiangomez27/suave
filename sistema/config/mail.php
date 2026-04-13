ad081c6d14f7a3ff40193e916f02e0866fa37b93
cristiangomez27 <Suaveurban2227@gmail.com>
2026-04-13 13:15:35 -0600

    Add files via upload

---
+++ sistema/config/mail.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!defined('MAIL_HOST')) define("MAIL_HOST", "smtp.hostinger.com");
+if (!defined('MAIL_PORT')) define("MAIL_PORT", 465);
+if (!defined('MAIL_USERNAME')) define("MAIL_USERNAME", "print@suaveurbanstudio.com.mx");
+if (!defined('MAIL_PASSWORD')) define("MAIL_PASSWORD", "Suavecolombia27@");
+if (!defined('MAIL_FROM_ADDRESS')) define("MAIL_FROM_ADDRESS", "print@suaveurbanstudio.com.mx");
+if (!defined('MAIL_FROM_NAME')) define("MAIL_FROM_NAME", "Suave Urban Studio");
+if (!defined('MAIL_ENCRYPTION')) define("MAIL_ENCRYPTION", "ssl");
+if (!defined('APP_URL')) define("APP_URL", "https://suaveurbanstudio.com.mx");
